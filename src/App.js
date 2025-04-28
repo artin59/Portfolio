5a384943c3b21408fad6546a202dfe7a6584e130
Artin Kiany <artinkiany59@gmail.com>
2025-04-27 21:50:14 -0400

    asd

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ function App() {
             <Footer />
           </>
         } />
-        <Route path="/project1" element={<Project1 />} />
-        <Route path="/project2" element={<Project2 />} />
-        <Route path="/project3" element={<Project3 />} />
-        <Route path="/project4" element={<Project4 />} />
+        <Route path="/Portfolio/project1" element={<Project1 />} />
+        <Route path="/Portfolio/project2" element={<Project2 />} />
+        <Route path="/Portfolio/project3" element={<Project3 />} />
+        <Route path="/Portfolio/project4" element={<Project4 />} />
       </Routes>
     </div>
   );
